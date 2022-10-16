8b652bc3a9b3f13701178b98a9314200aa221666
NI\micwu <michael.wu@ni.com>
2022-10-15 21:43:42 -0500

    Refactored code base

---
+++ MonMart_dotNet/Models/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MonMart.Models;
+using Npgsql;
+using System.Data;
+
+namespace MonMart.Controllers
+{
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        //private readonly IAuthService
+        public AuthenticationController()
+        {
+
+        }
+
+        [HttpPost("Login")]
+        public ActionResult Login(AuthenticationModel authModel)
+        {
+
+        }
+
+    }
+}
---
+++ MonMart_dotNet/Models/Models/AuthenticationModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MonMart.Models
+{
+    /// <summary>
+    /// Model for login.
+    /// </summary>
+    public class AuthenticationModel
+    {
+        /// <summary>
+        /// Username to login.
+        /// </summary>
+        public string UserName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Password to login.
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+    }
+}
