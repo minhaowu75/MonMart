38b9ec741301a8af68d19bc304ec10e6d48fd8b6
minhao.wu75@gmail.com <minhao.wu75@gmail.com>
2022-12-24 10:30:05 -0600

    created DTOs

--- MonMart_dotNet/Models/Controllers/AuthenticationController.cs
+++ MonMart_dotNet/Models/Controllers/AuthenticationController.cs
@@ -2,22 +2,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MonMart.Models;
+using MonMart.DTOs;
 using Npgsql;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Data;
 
 namespace MonMart.Controllers
 {
     [ApiController]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController : Controller
     {
-        //private readonly IAuthService
+        
         public AuthenticationController()
         {
 
         }
 
         [HttpPost("Login")]
-        public ActionResult Login(AuthenticationModel authModel)
+        public ActionResult Login(AuthenticationDTO authenticationDTO)
         {
 
         }
